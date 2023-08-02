f62722341e9df94bf04275bb44445f9ea6ddc0f2
Nathan-D-George <nathan.intern@bitcube.tech>
2023-08-02 16:01:34 +0200

    can do 2 function product rule

--- app/controllers/functions_controller.rb
+++ app/controllers/functions_controller.rb
@@ -29,58 +29,65 @@ class FunctionsController < ApplicationController
     
   def classify_terms(function)
     terms_hash = {}
-    final_ans  = []
-    # delimiters = ['+','-']
-    # terms_arr  = function.expression.split(Regexp.union(delimiters))
+    diff_ans  = []
     terms_arr = function.expression.split('+')
     terms_arr.each{ |term| 
       func = Function.new(expression: term)
       func.classify_function
       terms_hash[term] = func.classification
-      der = nil
-      if func.classification == "simple"
-        index = get_index(func.expression)
-        coef  = get_coefficient(func.expression)
-        const = false
-        func.expression.include?('x') ? const = false : const = true
-        der = differentiate_simple({coef: coef, index: index, constant: const})
-        final_ans.append(der[:func])
-      else
-        body  = get_body(func.expression).first
-        i_index = get_index(body)
-        i_coef  = get_coefficient(body)
-        expression = func.expression
-        if func.classification == "trigonometric"
-          typef = get_type(func.expression)
-          expression.remove!("#{typef}(#{body})")
-          o_index = get_index(expression)
-          o_coef  = get_coefficient(expression)
-          der = differentiate_trigonometric({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body, typef: typef})
-          final_ans.append(der[:func])
-        elsif func.classification == "exponential"
-          expression.remove!("exp","(#{body})")
-          o_index = get_index(expression)
-          o_coef  = get_coefficient(expression)
-          der = differentiate_exponential({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
-          final_ans.append(der[:func])
-        elsif func.classification == "natural_log"
-          expression.remove!("ln","(#{body})")
-          o_index = get_index(expression)
-          o_coef  = get_coefficient(expression)
-          der = differentiate_natural_log({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
-          final_ans.append(der[:func])
-        elsif func.classification == "logarithmic"
-          expression.remove!("log","(#{body})")
-          o_index = get_index(expression)
-          o_coef  = get_coefficient(expression)
-          der = differentiate_logarithmic({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
-          final_ans.append(der[:func])
-        end
-      end
-      
+      compound = compound_term?(term)
+      differentiate_product_rule(func) if !compound[:typef].blank? && compound[:typef] == "product-rule"
+      # debugger
+      # diff_ans.append(differentiate(func).first)
     } 
-    final_ans.delete("0")
-    final_ans.join('+')
+    # diff_ans.delete("0")
+    # diff_ans.each {|inst| inst.prepend('('); inst.concat(')')  }
+    # diff_ans.join('+')
+  end
+
+  def differentiate(func)
+    final_ans  = []
+    if func.classification == "simple"
+      index = get_index(func.expression)
+      coef  = get_coefficient(func.expression)
+      const = false
+      func.expression.include?('x') ? const = false : const = true
+      der = differentiate_simple({coef: coef, index: index, constant: const})
+      final_ans.append(der[:func])
+    else
+      body    = get_body(func.expression).first
+      i_index = get_index(body)
+      i_coef  = get_coefficient(body)
+      expression = func.expression
+      if func.classification == "trigonometric"
+        typef = get_type(func.expression)
+        expression.remove!("#{typef}(#{body})")
+        o_index = get_index(expression)
+        o_coef  = get_coefficient(expression)
+        der = differentiate_trigonometric({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body, typef: typef})
+        final_ans.append(der[:func])
+      elsif func.classification == "exponential"
+        expression.remove!("exp","(#{body})")
+        o_index = get_index(expression)
+        o_coef  = get_coefficient(expression)
+        der = differentiate_exponential({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
+        final_ans.append(der[:func])
+      elsif func.classification == "natural_log"
+        expression.remove!("ln","(#{body})")
+        o_index = get_index(expression)
+        o_coef  = get_coefficient(expression)
+        der = differentiate_natural_log({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
+        final_ans.append(der[:func])
+      elsif func.classification == "logarithmic"
+        expression.remove!("log","(#{body})")
+        o_index = get_index(expression)
+        o_coef  = get_coefficient(expression)
+        der = differentiate_logarithmic({o_coef: o_coef, i_coef: i_coef, o_index: o_index, i_index: i_index, body: body})
+        final_ans.append(der[:func])
+      end
+    end
+    final_ans
+    
   end
 
   def get_index(term)
@@ -113,6 +120,37 @@ class FunctionsController < ApplicationController
     ans
   end
 
+  def get_type(term)
+    return 'sin' if term.include?('sin')
+    return 'cos' if term.include?('cos')
+    return 'tan' if term.include?('tan')
+    return 'csc' if term.include?('csc')
+    return 'sec' if term.include?('sec')
+    return 'cot' if term.include?('cot')
+    return 'exp' if term.include?('exp')
+    return 'log' if term.include?('log')
+    return 'ln'  if term.include?('ln')
+  end
+
+  def compound_term?(term)
+    return {compound: false} if !term.include?('(')
+
+    term_parts = term.split('*')
+    term_num   = term_parts.length
+
+    if term_parts.length > 1
+      term_parts.each {|part| term_num -= 1 if is_integer?(part)}
+      term_num > 1 ? {compound: true, typef: 'product-rule'} : {compound: false}
+    elsif term_parts.length == 1
+      t = get_type(term)
+      if t == "sin" || t == "cos" || t == "tan" || t == "csc" || t == "sec" || t == "cot"
+        term_parts.first.include?('^') ? {compound: true, typef: 'chain-rule'} : {compound: false}
+      else
+        return {compound: false}
+      end
+    end
+  end
+
   def differentiate_simple(hash)
     return {func:'0'} if hash[:constant] 
     ans = ''
@@ -138,26 +176,11 @@ class FunctionsController < ApplicationController
       typef == "sin" ? func = "cos(#{hash[:body]})"                     : typef == "cos" ? func = "sin(#{hash[:body]})"                    : typef == "tan" ? func = "sec^2(#{hash[:body]})" : func.concat('')
       typef == "csc" ? func = "csc(#{hash[:body]})*cot(#{hash[:body]})" : typef == "sec" ? func = "sec(#{hash[:body]})*tan(#{hash[:body]})": typef == "cot" ? func = "csc^2(#{hash[:body]})" : func.concat('')
       ans   = {func: func.prepend((first.first.to_i*hash[:o_coef].to_i).to_s)}
-      ans   = {func: func.prepend('-')} if typef == "cos" || typef == "csc" || typef = "cot"
-      var   = (first.first.to_i*hash[:o_coef].to_i).to_s
-      # .concat(forst[:func].remove!(first.first))
-      debugger
+      ans   = {func: func.prepend('-')} if typef == "cos" || typef == "csc" || typef == "cot"
     else
       ans = {}
     end
     ans
-  end
-
-  def get_type(term)
-    return 'sin' if term.include?('sin')
-    return 'cos' if term.include?('cos')
-    return 'tan' if term.include?('tan')
-    return 'csc' if term.include?('csc')
-    return 'sec' if term.include?('sec')
-    return 'cot' if term.include?('cot')
-    return 'exp' if term.include?('exp')
-    return 'log' if term.include?('log')
-    return 'ln'  if term.include?('ln')
   end
 
   def differentiate_exponential(hash)
@@ -191,7 +214,18 @@ class FunctionsController < ApplicationController
     ans = {func: "#{ln_ans_parts.first}/#{ln_ans_parts.last}"}
   end
 
-  def differentiate_product_rule
+  def differentiate_product_rule(func)
+    term_parts = func.expression.split('*')
+    differentials = []
+    term_parts.each {|part|
+      sub_func = Function.new(expression: part)
+      sub_func.classify_function
+      differentials.append(differentiate(sub_func).first)
+    }
+    term_parts.each    {|t| t.prepend('('); t.concat(')')}
+    differentials.each {|d| d.prepend('('); d.concat(')')}
+    ans = "#{term_parts.first}*#{differentials.last} + #{term_parts.last}*#{differentials.first}"
+    debugger
   end
 
   def differentiate_quotient_rule
@@ -200,8 +234,7 @@ class FunctionsController < ApplicationController
   def differentiate_chain_rule
   end
 
-  def multiple_functions?(expression)
-  end
+
 
   def string_to_array(string)
     array = []
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,28 @@ Rails.application.routes.draw do
 
 end
 =begin
+  More than Able
+  Firm Foundation
   Jehovah
   Yahweh
   Yahweh will Manifest
   Yeshua
   Who is this man?
   Good (can't be anything else)
+
+
+  simple:
+    x^2 + 3ln(x) - sin(2x)
+    exp(4x^2)
+
+  product-rule:
+    ln(x) * sin(2x)
+    2x * exp(4x)
+     x * exp(x)
+
+  chain-rule:
+    sin(3x^2)
+    sin^2(3x)
+
 =end
+
